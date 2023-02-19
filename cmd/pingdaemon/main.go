1dd67d122792fba1cf7ba564a2323b39af5b37ea
Nikolay Barabanov <nbarabanov@mtt.ru>
2023-02-19 22:36:52 +0300

    resolve domain names

--- cmd/pingdaemon/main.go
+++ cmd/pingdaemon/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io"
+	"net"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
 
 	"gopkg.in/yaml.v3"
 
@@ -13,7 +18,14 @@ import (
 )
 
 type Config struct {
-	URL string `yaml:"url"`
+	URI    string   `yaml:"uri"`
+	Domain string   `yaml:"domain"`
+	Port   int      `yaml:"port"`
+	Hosts  []string `yaml:"hosts"`
+}
+
+func init() {
+	net.DefaultResolver.PreferGo = true
 }
 
 func main() {
@@ -36,7 +48,58 @@ func main() {
 
 	logger := logger.New(file)
 
-	resp, err := http.Get(config.URL)
+	dialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+	http.DefaultTransport.(*http.Transport).DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		if addr == fmt.Sprintf("%v:%v", config.Domain, config.Port) {
+			addr = fmt.Sprintf("%v:%v", config.Hosts[0], config.Port)
+		}
+		return dialer.DialContext(ctx, network, addr)
+	}
+
+	headers := http.Header{}
+	headers.Add("User-Agent", "SitePingerDaemon/0.1")
+
+	requestUrl, err := url.Parse(config.URI)
+	if err != nil {
+		log.Print(err)
+	}
+
+	request := http.Request{
+		Method:           http.MethodGet,
+		URL:              requestUrl,
+		Proto:            "",
+		ProtoMajor:       0,
+		ProtoMinor:       0,
+		Header:           headers,
+		Body:             nil,
+		GetBody:          nil,
+		ContentLength:    0,
+		TransferEncoding: nil,
+		Close:            false,
+		Host:             "",
+		Form:             nil,
+		PostForm:         nil,
+		MultipartForm:    nil,
+		Trailer:          nil,
+		RemoteAddr:       "",
+		RequestURI:       "",
+		TLS:              nil,
+		Cancel:           nil,
+		Response:         nil,
+	}
+
+	client := http.Client{
+		Transport:     nil,
+		CheckRedirect: nil,
+		Jar:           nil,
+		Timeout:       0,
+	}
+	resp, err := client.Do(&request)
+
+	//resp, err := http.Get(config.URL)
 	if err != nil {
 		log.Print(err)
 	}
