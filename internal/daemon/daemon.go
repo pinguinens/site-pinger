cf5179fc057082d7edb15c935d1c9910ba41f0bd
Nikolay Barabanov <nbarabanov@mtt.ru>
2023-02-27 22:06:26 +0300

    daemon creates clients per resource

--- internal/daemon/daemon.go
+++ internal/daemon/daemon.go
@@ -1,9 +1,13 @@
 package daemon
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"net"
+	"net/http"
 	"net/url"
+	"strings"
 
 	log "github.com/rs/zerolog"
 
@@ -26,6 +30,14 @@ func New(logger *log.Logger, sites []site.Site, dialer *net.Dialer) Daemon {
 		}
 
 		for _, h := range s.Target.Hosts {
+			transport := http.Transport{
+				DialContext: makeDialContext(uri.Host, h, dialer),
+			}
+
+			client := &http.Client{
+				Transport: &transport,
+			}
+
 			resources = append(resources, resource.New(
 				s.Target.Method,
 				s.Target.URI,
@@ -33,7 +45,7 @@ func New(logger *log.Logger, sites []site.Site, dialer *net.Dialer) Daemon {
 					Domain: uri.Host,
 					Addr:   h,
 				},
-				dialer),
+				client),
 			)
 		}
 	}
@@ -58,5 +70,17 @@ func (d *Daemon) Start() {
 
 			d.logger.Info().Int("status_code", response.StatusCode).Msg(string(body))
 		}
+	}
+}
+
+func makeDialContext(domain, host string, dialer *net.Dialer) func(ctx context.Context, network, addr string) (net.Conn, error) {
+	return func(ctx context.Context, network, addr string) (net.Conn, error) {
+		parts := strings.Split(addr, ":")
+
+		if addr == fmt.Sprintf("%v:%v", domain, parts[1]) {
+			addr = fmt.Sprintf("%v:%v", host, parts[1])
+		}
+
+		return dialer.DialContext(ctx, network, addr)
 	}
 }
--- internal/resource/resource.go
+++ internal/resource/resource.go
@@ -1,9 +1,6 @@
 package resource
 
 import (
-	"context"
-	"fmt"
-	"net"
 	"net/http"
 	"net/url"
 	"strings"
@@ -13,37 +10,19 @@ type Resource struct {
 	Method string
 	URI    string
 	Host   Host
-	dialer *net.Dialer
+	client *http.Client
 }
 
-func New(method, uri string, host Host, dialer *net.Dialer) Resource {
+func New(method, uri string, host Host, client *http.Client) Resource {
 	return Resource{
 		Method: method,
 		URI:    uri,
 		Host:   host,
-		dialer: dialer,
+		client: client,
 	}
 }
 
 func (r *Resource) Ping() (*http.Response, error) {
-	dcFunc := func(ctx context.Context, network, addr string) (net.Conn, error) {
-		parts := strings.Split(addr, ":")
-
-		if addr == fmt.Sprintf("%v:%v", r.Host.Domain, parts[1]) {
-			addr = fmt.Sprintf("%v:%v", r.Host.Addr, parts[1])
-		}
-
-		return r.dialer.DialContext(ctx, network, addr)
-	}
-
-	transport := &http.Transport{
-		DialContext: dcFunc,
-	}
-
-	client := http.Client{
-		Transport: transport,
-	}
-
 	headers := http.Header{}
 	headers.Add("User-Agent", "SitePingerDaemon/0.1")
 
@@ -58,7 +37,7 @@ func (r *Resource) Ping() (*http.Response, error) {
 		Header: headers,
 	}
 
-	resp, err := client.Do(&request)
+	resp, err := r.client.Do(&request)
 	if err != nil {
 		return nil, err
 	}
